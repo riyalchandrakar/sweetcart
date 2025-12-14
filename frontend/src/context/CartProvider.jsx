4fcd69fdb39cae45b71a43551746cbfd12180f22
riyalchandrakar <riyalchandrakar6@gmail.com>
2025-12-14 19:27:11 +0530

    fix: cart context filename casing

--- frontend/src/context/CartProvider.jsx
+++ frontend/src/context/CartProvider.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { CartContext } from "./cartContext";
+import { CartContext } from "./CartContext";
 
 const CART_KEY = "sweetshop_cart";
 
--- frontend/src/context/useCart.js
+++ frontend/src/context/useCart.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { CartContext } from "./cartContext";
+import { CartContext } from "./CartContext";
 
 const useCart = () => {
   return useContext(CartContext);
